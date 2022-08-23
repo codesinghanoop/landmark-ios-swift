73d99788187257f17fd802c6a1ee9c71b26a64a0
ANOOP SINGH <anoop.singh@tradeling.com>
2022-08-23 10:51:00 +0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Anoop Singh on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
